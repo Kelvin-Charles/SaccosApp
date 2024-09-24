e499f74e1a0def72d390d006c2a8f43088467a31
kelvin-Charles <kvoodec@gmail.com>
2024-09-24 23:45:14 +0300

    minor changes

--- saccos-backend/server.js
+++ saccos-backend/server.js
@@ -28,7 +28,7 @@ db.connect(err => {
 
 // JWT Secret tokea kwenye .env file
 
-const JWT_SECRET = process.env.JWT_SECRET;
+const JWT_SECRET = "6a079d7096ea08a3faddf070831d19a5eb575fb2d7b721f46996c06a07e036a407f265dcad429cdaf7e10c2964f42f9f8a36649e0909e64c5464b8d238c91751"//process.env.JWT_SECRET;
 
 
 // Endpoint ya user kulogin
